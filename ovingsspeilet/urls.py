937e8ca7ad10ed15083e6dbd93ad576b3d04405a
Johan Reitan <johan.reitan@gmail.com>
2015-04-05 23:42:35 +0200

    securify!

--- ovingsspeilet/urls.py
+++ ovingsspeilet/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from ovingsspeilet import views
 
 urlpatterns = [
     url(r'^calendar', views.calendar, name='calendar'),
-    url(r'^$', TemplateView.as_view(template_name="ovingsspeilet/rehearsal.html"), name='rehearsal'),
+    url(r'^$', login_required(TemplateView.as_view(template_name="ovingsspeilet/rehearsal.html")), name='rehearsal'),
 ]
