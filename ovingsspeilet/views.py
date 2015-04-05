6813c7ce754d9eb5bbf03259fe801b64ab6c63ee
Johan Reitan <johan.reitan@gmail.com>
2015-04-05 23:43:16 +0200

    fill the calendar with events from db

--- ovingsspeilet/views.py
+++ ovingsspeilet/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.utils import timezone
+from ovingsspeilet.models import Event
 
 
 @login_required
 def calendar(request):
-    return JsonResponse({'foo': 'bar'})
+    event_objects = Event.objects.all()
+    events = []
+    for event in event_objects:
+        events.append({
+            'id': event.id,
+            'title': event.title,
+            'start': timezone.localtime(event.date_start),
+            'end': timezone.localtime(event.date_end)
+        })
+    return JsonResponse(events, safe=False)
